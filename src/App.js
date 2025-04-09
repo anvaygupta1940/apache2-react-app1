75864d69fd0f3bf90ea03d8502af877661b30b84
anvaygupta1940 <guptaanvay1940@gmail.com>
2025-04-09 15:12:19 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is react application 1 . build to be served by apache2 web server
         </p>
         <a
           className="App-link"
